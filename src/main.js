e7b8761a84f3ee6d1ef9b3e7a8faccef8326c568
FRANCISCO ANDRES ESPINOSA LILLO <f.espinosa01@ufromail.cl>
2022-07-09 22:03:45 -0400

    Se eliminaron archivos creados por defecto al instalar vue

--- src/main.js
+++ src/main.js
@@ -4,12 +4,9 @@ import router from "./router";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import axios from "axios";
 import "chart.js";
-
 const app = createApp(App);
 
-app.use(axios);
 app.use(router);
 
 app.mount("#app");
