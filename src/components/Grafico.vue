d71cd4a127497bb964e43c135b275c3093b0b2c5
Andr3sCid <a.cid04@ufromail.cl>
2022-07-10 02:04:51 -0400

    Se quetan cosas innecesarias de momento y se comenta el código

--- src/components/Grafico.vue
+++ src/components/Grafico.vue
@@ -17,22 +17,25 @@ export default {
   metods: {
   },
   mounted() {
-    //obtener datos de la base de datos
-    axios.get('https://7qak3a37b4dh7kisebhllubdxq0dnehm.lambda-url.us-east-1.on.aws/').then(response => this.sesiones = response.data);
-    console.log(this.sesiones);
     //Gráfico
     const ctx = document.getElementById("graficoSesiones");
     const graficoSesiones = new Chart(ctx, {
+      //Definimos el tipo de grafico
       type: "line",
       data: {
+        //Las variables de la barra x
         labels: ["12/09/2022", "13/09/2022", "14/09/2022", "15/09/2022", "16/09/2022", "17/09/2022", "18/09/2022", "19/09/2022", "20/09/2022", "21/09/2022"],
         datasets: [
           {
+            //Nombre de la gráfica
             label: "Rendimiento por sesión",
+            //Los datos visualizados en la gráfica
             data: [1.2, 1.9, 3, 4.5, 2, 3.3, 4, 5.6, 2, 1,1],
+            //color de relleno de los datos
             backgroundColor: [
               "rgba(68, 196, 175, 0.5)",
             ],
+            //Color del borde de los datos
             borderColor: [
               "rgba(18, 103, 138, 1)",
             ],
@@ -43,11 +46,13 @@ export default {
       options: {
         scales: {
           y: {
+            //Configurción para que comienze desde el valor 0 en la gráfica en el eje y
             beginAtZero: true,
           },
         },
       },
     });
+    //Se inicia la gráfica
     graficoSesiones;
     /*
     console.log("wena los k")
