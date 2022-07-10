2694f428094ec08da503d681508e9f29b797eef7
FRANCISCO ANDRES ESPINOSA LILLO <f.espinosa01@ufromail.cl>
2022-07-09 20:47:09 -0400

    Se corrige unos imports en main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import bootstrap from "bootstrap";
+import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import axios from "axios";
-import chart from "chart.js";
+import "chart.js";
 
 const app = createApp(App);
 
-app.use(chart);
 app.use(axios);
-app.use(bootstrap);
 app.use(router);
 
 app.mount("#app");
