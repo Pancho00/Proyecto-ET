8e929f04a771d705050fa35f8aabec92a2ca34b1
FRANCISCO ANDRES ESPINOSA LILLO <f.espinosa01@ufromail.cl>
2022-07-10 01:43:36 -0400

    Se termina la barra superior y se agrega la inferior (footer)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import BarraSuperior from "./components/BarraSuperior.vue";
+import BarraInferior from "./components/BarraInferior.vue";
 </script>
 
 <template>
-<h1>holiwui</h1>
+  <BarraSuperior />
+  <BarraInferior />
 </template>
 
 <style>
-
 #app {
   max-width: 1280px;
   margin: 0 auto;
@@ -15,5 +17,4 @@ import { RouterLink, RouterView } from "vue-router";
 
   font-weight: normal;
 }
-
 </style>
